3702cacbdb12f19faf31f22efe79aee4f7c3569b
AJ Hephner <ajhephner178@gmail.com>
2017-09-08 14:03:42 -0400

    got re-log to work

--- app.js
+++ app.js
@@ -3,66 +3,93 @@ $(function(){
       var API_BASE = "https://ajh-workoutlog-server.herokuapp.com/api/";
       var userDefinitions = [];
 
-      var setAuthHeader = function(sessionToken){
-        window.localStorage.setItem("sessionToken", sessionToken);
-        //Set the authorization header
-        //This can be done on individual calls
-        //here we showcase ajaxSetup as a global tool 
-        $.ajaxSetup({
-          "headers": {
-            "Authorization": sessionToken
-          }
-        });
+      var setAuthHeader = function(sessionToken) {
+         window.localStorage.setItem("sessionToken", sessionToken);
+         // Set the authorization header
+         // This can be done on individual calls
+         // here we showcase ajaxSetup as a global tool
+         $.ajaxSetup({
+            "headers": {
+               "Authorization": sessionToken
+            }
+         });
       };
-        //public
-        return {
-          API_BASE: API_BASE, 
-          setAuthHeader: setAuthHeader
-        };
 
-    })(jQuery);
-        // Ensure .disable aren't clickable
-        $('.nav-tabs a[data-toggle="tab"]').on("click", function(e){
-          var token = window.localStorage.getItem("sessionToken");
-          if ($(this).hasClass("disabled")&& !token) {
-            e.preventDefault();
-            return false;
-          }
-        });
+      // public
+      return {
+         API_BASE: API_BASE,
+         setAuthHeader: setAuthHeader
+      };
+   })(jQuery);
 
-        //blind tab change events
-        $('a[data-toggle="tab"]').on('shown.bs.tab', function (e){
-          var target = $(e.target).attr("href"); //activated tab
-          if (target === "#log") {
-            WorkoutLog.log.setDefinitions();
-          }
-           if (target === "#update-log") {
+   // Ensure .disabled aren't clickable
+   $(".nav-tabs a[data-toggle=tab]").on("click", function(e) {
+      var token = window.localStorage.getItem("sessionToken");
+      if ($(this).hasClass("disabled") && !token) {
+         e.preventDefault();
+         return false;
+      }
+   });
+
+   //bind tab change events
+   $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      var target = $(e.target).attr("href"); // activated tab
+      if (target === "#log") {
+         WorkoutLog.log.setDefinitions();
+      }
+
+      if (target === "#update-log") {
         WorkoutLog.log.setDefinitions();
       }
 
-          if (target === "#history"){
-            WorkoutLog.log.setHistory();
-          }
-        });
-        //bind enter key
-        $(document).on("keypress", function(e){
-          if (e.which === 13) {
-            if ($("#signup-modal").is(":visible")){
-              $("#signup").trigger("click");
-            }
+      if (target === "#history") {
+         WorkoutLog.log.setHistory();
+      }
+   });
 
-            if($("#login-modal").is(":visible")){
-              $("#login").trigger("click");
-            }
-          }
-        });
-        //setHeader if we
-        var token = window.localStorage.getItem("sessionToken");
-        if (token){
-          WorkoutLog.setAuthHeader(token);
-        }
+   // bind enter key
+   $(document).on("keypress", function(e) {
+      if (e.which === 13) { // enter key
+         if ($("#signup-modal").is(":visible")) {
+            $("#signup").trigger("click");
+         }
+         if ($("#login-modal").is(":visible")) {
+            $("#login").trigger("click");
+         }
+      }
+   });
 
-      //expose this to the other workoutlog modules
-      window.WorkoutLog = WorkoutLog;
+  //  bind tab change events
+  // bootstrap tab --> binding to a bootstrap event
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+     var target = $(e.target).attr("href"); // activated tab
+     if (target === "#log") {
+        WorkoutLog.log.setDefinitions();
+     }
 
+     if (target === "#history") {
+        WorkoutLog.log.setHistory();
+     }
   });
+
+  // bind enter key
+   $(document).on("keypress", function(e) {
+      if (e.which === 13) { // enter key
+         if ($("#signup-modal").is(":visible")) {
+            $("#signup").trigger("click");
+         }
+         if ($("#login-modal").is(":visible")) {
+            $("#login").trigger("click");
+         }
+      }
+   });
+
+   // setHeader if we
+   var token = window.localStorage.getItem("sessionToken");
+   if (token) {
+      WorkoutLog.setAuthHeader(token); 
+   }
+
+   // expose this to the other workoutlog modules
+   window.WorkoutLog = WorkoutLog;
+});
