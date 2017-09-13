bde487b988dfee64673feefadea295ad1e6eb392
AJ Hephner <ajhephner178@gmail.com>
2017-09-13 13:37:31 -0400

    added dropdown and changed some styles

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	alert("JQuery is active");
+
+$('form').submit(function(){
+	var name = $.trim($('#su_username').val());
+
+	if (name === ''){
+		alert('Need a name');
+		return false
+	},
+
+
+	
+
+
+
+
+});
